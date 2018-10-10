b209feaf64b253acd3e6ae8d57ddef6e1517d6c7
Zak <zakaria.abouelhouda@gmail.com>
2018-10-10 17:14:04 +0200

    mocha unit test added

--- test/calculator_test.js
+++ test/calculator_test.js
@@ -21,9 +21,27 @@ contract('CalculatorTest', function(accounts) {
         return calculator_instance.getNumber.call();
       }).then((results)=>{
         assert.equal(results.valueOf(),110,"Congratulations");
-       
-      });
-         
+        return calculator_instance.substractFromStateNumber(5);   
+      }).then(()=>{
+        return calculator_instance.getNumber.call();
+      }).then((resultas)=>{
+        assert.equal(resultas.valueOf(),105,"Congratulations");
       });
     });
-
+      it("should return 105 when call to add/subtract for ((Initial_Value * 100) / 2)", ()=> {
+        var calculator_instance;
+        return Calculator.deployed().then((instance)=>{
+          calculator_instance=instance;
+          return calculator_instance.multiplyWithNumber(100);   
+        }).then(()=>{
+          return calculator_instance.getNumber.call();
+        }).then((resultas)=>{
+          assert.equal(resultas.valueOf(),10500,"Congratulations");
+          return calculator_instance.divideNumberBy(2); 
+        }).then(()=>{
+          return calculator_instance.getNumber.call();
+        }).then((resultas)=>{
+          assert.equal(resultas.valueOf(),5250,"Congratulations");
+        });
+    });
+  });
